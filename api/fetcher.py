94f9d7c96638c717ab9ff50c8c69f5fe4ad958a7
zenn <lars.gajew@googlemail.com>
2021-08-20 20:19:25 +0200

    fetch logs all minions with a tavern tier in fetch.json

--- api/fetcher.py
+++ api/fetcher.py
@@ -1,4 +1,5 @@
 import requests
+from json import dumps
 
 class Fetcher:
     api_link: str = "https://api.hearthstonejson.com/v1/91040/enUS/cards.json"
@@ -9,15 +10,21 @@ class Fetcher:
             api_link = Fetcher.api_link
 
         # curl
-        json = requests.get(api_link).json()
+        json = requests.get(api_link).json()  # type: list
 
         for card in json:  # type: dict
-            if card.get("battlegroundsPremiumDbfId") is not None:
+            if card.get("techLevel") is not None:
                 text: str = card.get("text")
                 if text is not None:
                     text = text.replace("\n", " ")
 
-                print(f'{card.get("name")} - {text}')
+                print(f'{card.get("techLevel")} - {card.get("name")} - {text}')
+
+            else:
+                json.remove(card)
+
+        with open("fetch.json", "w+") as fetch:
+            fetch.write(dumps(json, indent=2))
 
 
 if __name__ == "__main__":
